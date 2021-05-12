f62ebd82ddf5643c7317376f9252d71ef70f31dd
unknown <sam1919>
2021-05-12 11:16:33 +0530

    Employee Attendance

--- Employy.java
+++ Employy.java
@@ -4,5 +4,18 @@ public class Employy {
 public static void main(String[] args) 
 {
 	System.out.println("Wellcom to the Employee Wage Builder");
+
+	int x = 1;
+	int y = 0;
+	if (x > y) 
+	{
+		System.out.println("Employee is Present");
+	}
+	else 
+		{
+		System.out.println("Employee is Absent");
+		}
+	
 }
+
 }
